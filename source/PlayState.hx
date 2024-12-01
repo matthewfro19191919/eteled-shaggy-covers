58671d965fa9ef407097c6ffefd8c5d49473846b
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-01 16:01:33 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3769,7 +3769,6 @@ class PlayState extends MusicBeatState
 
 					if (SONG.validScore)
 					{
-						NGio.unlockMedal(60961);
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 					}
 
