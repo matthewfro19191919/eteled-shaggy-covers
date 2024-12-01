2525785e6714002e3c6332fc3109c947a8e98b62
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-01 14:48:46 -0500

    Create windows.bat

---
+++ setup/windows.bat
@@ -0,0 +1,25 @@
+@echo off
+color 0a
+cd ..
+@echo on
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 4.11.0
+haxelib install flixel-addons 2.11.0
+haxelib install flixel-tools
+haxelib install flixel-ui 2.6.1
+haxelib install actuate 1.9.0 
+haxelib install hxCodec 2.5.1          
+haxelib install linc_luajit
+haxelib install hscript
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+haxelib install hxcpp-debug-server
+echo Finished!
+pause
